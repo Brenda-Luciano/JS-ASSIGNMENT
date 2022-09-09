0a3c4081b05391bdcd3b25c9ff613abf84642247
Brenda-Luciano <107413757+Brenda-Luciano@users.noreply.github.com>
2022-09-09 17:10:41 -0300

    Delete assignment 2 directory

--- assignment 2/a/main.js
+++
@@ -1,6 +0,0 @@
-function calculateDiagonal(){
-	var side = document.getElementById('side').value
-	var square = Math.sqrt(2)
-	var diagonal = Number(side) * square
-	document.getElementById('answer').innerHTML = "The typed value was: " + side + " and the diagonal is: " + diagonal
-}
